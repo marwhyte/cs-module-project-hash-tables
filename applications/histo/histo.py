c04c6a0056e0dae7dfffd51d48e5b471c89bcb65
Marco Whyte <mwhyte@umass.edu>
2020-07-09 13:01:37 -0500

    finished histo

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,43 @@
 # Your code here
 
+f = open("robin.txt", "r")
+removeChars = ["\"", ":", ";", ",", ".", "-", "+", "=", "/", "\\",
+               "|", "[", "]", "{", "}", "(", ")", "*", "^", "&", '', "!", "?"]
+space = " "
+
+cache = {}
+word = ""
+
+for x in f.read().lower():
+    if x in removeChars:
+        continue
+
+    if x == space or x == "\n":
+        if word in cache:
+            cache[word] += 1
+        else:
+            cache[word] = 1
+        word = ""
+    else:
+        word += x
+
+
+print(cache)
+
+a = sorted(cache.items(), key=lambda keyAndVal: (keyAndVal[1], keyAndVal[0]))
+
+a = a[::-1]
+# print(a)
+str = ""
+numberOfSpaces = 20
+
+for item in a:
+    str += item[0]
+
+    for i in range(0, numberOfSpaces - len(str)):
+        str += " "
+
+    for i in range(0, item[1]):
+        str += "*"
+    print(str)
+    str = ""
