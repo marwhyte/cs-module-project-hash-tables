444eec236f48a2f82ed50bb14aa42605154f0e03
Marco Whyte <mwhyte@umass.edu>
2020-07-09 12:46:26 -0500

    finished ceasar

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,4 +2,32 @@
 # decode it.
 
 # Your code here
+finalHash = {}
+letterCount = {}
+finalText = ""
 
+with open("ciphertext.txt", encoding="utf-8") as cipherText:
+    cipher = cipherText.read()
+
+topLetters = ["E", "T", "A", "O", "H", "N", "R", "I", "S", "D", "L",
+              "W", "U", "G", "F", "B", "M", "Y", "C", "P", "K", "V", "Q", "J", "X", "Z"]
+
+for letter in cipher:
+    if letter.isalpha():
+        if letter not in letterCount:
+            letterCount[letter] = 1
+        else:
+            letterCount[letter] += 1
+
+sortThisList = list(letterCount.items())
+sortThisList.sort(reverse=True, key=lambda item: item[1])
+print(sortThisList)
+for i, element in enumerate(topLetters):
+    finalHash[sortThisList[i][0]] = topLetters[i]
+
+for letter in cipher:
+    if letter.isalpha():
+        finalText += finalHash[letter]
+    else:
+        finalText += letter
+print(finalText)
